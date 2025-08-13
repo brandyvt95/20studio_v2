e266598e6ecc91c5edb4824e86ecc1f33a0e9b0b
Dannyvt95 <brandy.hontran.dev@gmail.com>
2025-08-13 14:23:28 +0700

    g

--- composables/hooks/transitionpage/useMotionTransPage.ts
+++ composables/hooks/transitionpage/useMotionTransPage.ts
@@ -18,60 +18,60 @@ const handleTransition = (element: Element, className: string, duration: number,
         callback?.();
         element.classList.remove(className);
         clearTimeout(timer);
-    }, 20 * 1000);
+    }, 2 * 1000);
 }
 
-// export const motionEnterPage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
-//     const mainWrap = el.children[0];
-//     handleTransition(el, 'mask-enter', transitionMotion.duration, onCompleteCallback);
-//     handleTransition(mainWrap, 'page-enter', transitionMotion.duration, null);
-// }
-
-// export const motionLeavePage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
-//     const mainWrap = el.children[0];
-//     handleTransition(mainWrap, 'page-leave', transitionMotion.duration, onCompleteCallback);
-// }
-
-
 export const motionEnterPage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
-    const ctx = gsap.context(() => {
-        const mainWrap = el.children[0]
-        gsap.timeline()
-           //.set(el, { zIndex: setupZIndex() })
-            .fromTo(el,
-                transitionConfig.mask.start,
-                {
-                    ...transitionConfig.mask.end,
-                    onComplete: () => {
-                        onCompleteCallback?.()
-                        ctx.revert();
-                    }
-                }
-            )
-            .fromTo(mainWrap,
-                transitionConfig.mainWrap.enter.start,
-                transitionConfig.mainWrap.enter.end,
-                "<"
-            )
-    }, el)
+    const mainWrap = el.children[0];
+    handleTransition(el, 'mask-enter', transitionMotion.duration, onCompleteCallback);
+    handleTransition(mainWrap, 'page-enter', transitionMotion.duration, null);
 }
 
 export const motionLeavePage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
-    const ctx = gsap.context(() => {
-        const mainWrap = el.children[0]
-        gsap.to(
-            mainWrap,
-            //transitionConfig.mainWrap.leave.start,
-            {
-                ...transitionConfig.mainWrap.leave.end,
-                onComplete: () => {
-                    onCompleteCallback?.()
-                    ctx.revert() 
-                }
-            }
-        )
-    }, el)
+    const mainWrap = el.children[0];
+    handleTransition(mainWrap, 'page-leave', transitionMotion.duration, onCompleteCallback);
 }
+
+
+// export const motionEnterPage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
+//     const ctx = gsap.context(() => {
+//         const mainWrap = el.children[0]
+//         gsap.timeline()
+//            //.set(el, { zIndex: setupZIndex() })
+//             .fromTo(el,
+//                 transitionConfig.mask.start,
+//                 {
+//                     ...transitionConfig.mask.end,
+//                     onComplete: () => {
+//                         onCompleteCallback?.()
+//                         ctx.revert();
+//                     }
+//                 }
+//             )
+//             .fromTo(mainWrap,
+//                 transitionConfig.mainWrap.enter.start,
+//                 transitionConfig.mainWrap.enter.end,
+//                 "<"
+//             )
+//     }, el)
+// }
+
+// export const motionLeavePage = ({ el, onCompleteCallback }: IHandleMotionTransPage): void => {
+//     const ctx = gsap.context(() => {
+//         const mainWrap = el.children[0]
+//         gsap.to(
+//             mainWrap,
+//             //transitionConfig.mainWrap.leave.start,
+//             {
+//                 ...transitionConfig.mainWrap.leave.end,
+//                 onComplete: () => {
+//                     onCompleteCallback?.()
+//                     ctx.revert() 
+//                 }
+//             }
+//         )
+//     }, el)
+// }
 
 
 const handleTransitionDetailProject = (duration: number, callback: any) => {
